252503edffa34f7877acf6cce6b4d539fb7e5d5a
José Luís <peressousa77@gmail.com>
2023-09-26 21:36:41 -0300

    🔨 refactor: adicionado uma estrutura de pastas base

---
+++ front-codex/src/app/(public)/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Home Page - Codex',
+    description: ''
+};
+
+export default function RootLayout({
+    children
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="pt-br">
+            <body>{children}</body>
+        </html>
+    );
+}
